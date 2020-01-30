0af7780ce38a7c023dcc56b8c6597e592fc60f55
apple <apple@apple.com>
2020-01-31 01:13:10 +0800

    初始化6.13.0版本

---
+++ SiteServer.CMS/DataCache/Content/ContentManager.ListCache.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SiteServer.CMS.Core;
+using SiteServer.CMS.DataCache.Core;
+using SiteServer.CMS.Model;
+using SiteServer.Utils;
+using SiteServer.Utils.Enumerations;
+
+namespace SiteServer.CMS.DataCache.Content
+{
+    public static partial class ContentManager
+    {
+        private static class ListCache
+        {
+            private static readonly object LockObject = new object();
+            private static readonly string CachePrefix = DataCacheManager.GetCacheKey(nameof(ContentManager), nameof(ListCache));
+
+            private static string GetCacheKey(int channelId, int adminId)
+            {
+                return $"{CachePrefix}.{channelId}.{adminId}";
+            }
+
+            public static void Remove(int channelId)
+            {
+                lock(LockObject)
+                {
+                    DataCacheManager.RemoveByPrefix($"{CachePrefix}.{channelId}.");
+                }
+            }
+
+            public static List<int> GetContentIdList(int channelId, int adminId)
+            {
+                lock (LockObject)
+                {
+                    var cacheKey = GetCacheKey(channelId, adminId);
+                    var list = DataCacheManager.Get<List<int>>(cacheKey);
+                    if (list != null) return list;
+
+                    list = new List<int>();
+                    DataCacheManager.Insert(cacheKey, list);
+                    return list;
+                }
+            }
+
+            public static void Set(ContentInfo contentInfo)
+            {
+                var contentIdList = GetContentIdList(contentInfo.ChannelId, 0);
+                if (!contentIdList.Contains(contentInfo.Id))
+                {
+                    contentIdList.Add(contentInfo.Id);
+                }
+
+                contentIdList = GetContentIdList(contentInfo.ChannelId, contentInfo.AdminId);
+                if (!contentIdList.Contains(contentInfo.Id))
+                {
+                    contentIdList.Add(contentInfo.Id);
+                }
+            }
+        }
+
+        public static List<(int ChannelId, int ContentId)> GetChannelContentIdList(SiteInfo siteInfo, ChannelInfo channelInfo, int adminId, bool isAllContents, int offset, int limit)
+        {
+            var tableName = ChannelManager.GetTableName(siteInfo, channelInfo);
+
+            var channelContentIdList = new List<(int ChannelId, int ContentId)>();
+            foreach (var contentId in ListCache.GetContentIdList(channelInfo.Id, adminId))
+            {
+                channelContentIdList.Add((channelInfo.Id, contentId));
+            }
+            if (isAllContents)
+            {
+                var channelIdList = ChannelManager.GetChannelIdList(channelInfo, EScopeType.Descendant);
+                foreach (var contentChannelId in channelIdList)
+                {
+                    var contentChannelInfo = ChannelManager.GetChannelInfo(siteInfo.Id, contentChannelId);
+                    var channelTableName = ChannelManager.GetTableName(siteInfo, contentChannelInfo);
+                    if (!StringUtils.EqualsIgnoreCase(tableName, channelTableName)) continue;
+
+                    foreach (var contentId in ListCache.GetContentIdList(contentChannelId, adminId))
+                    {
+                        channelContentIdList.Add((contentChannelId, contentId));
+                    }
+                }
+            }
+
+            if (channelContentIdList.Count >= offset + limit)
+            {
+                return channelContentIdList.Skip(offset).Take(limit).ToList();
+            }
+
+            if (channelContentIdList.Count == offset)
+            {
+                var dict = ContentCache.GetContentDict(channelInfo.Id);
+
+                var pageContentInfoList = DataProvider.ContentDao.GetContentInfoList(tableName, DataProvider.ContentDao.GetCacheWhereString(siteInfo, channelInfo, adminId, isAllContents),
+                    DataProvider.ContentDao.GetOrderString(channelInfo, string.Empty, isAllContents), offset, limit);
+
+                foreach (var contentInfo in pageContentInfoList)
+                {
+                    ListCache.Set(contentInfo);
+                    dict[contentInfo.Id] = contentInfo;
+                }
+
+                var pageContentIdList = pageContentInfoList.Select(x => (x.ChannelId, x.Id)).ToList();
+                channelContentIdList.AddRange(pageContentIdList);
+                return pageContentIdList;
+            }
+
+            return DataProvider.ContentDao.GetCacheChannelContentIdList(tableName, DataProvider.ContentDao.GetCacheWhereString(siteInfo, channelInfo, adminId, isAllContents),
+                DataProvider.ContentDao.GetOrderString(channelInfo, string.Empty, isAllContents), offset, limit);
+        }
+    }
+}
---
+++ SiteServer.Web/Controllers/Pages/Cms/PagesContentsController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using NSwag.Annotations;
+using SiteServer.CMS.Core;
+using SiteServer.CMS.Core.Create;
+using SiteServer.CMS.DataCache;
+using SiteServer.CMS.DataCache.Content;
+using SiteServer.CMS.Model;
+using SiteServer.CMS.Plugin;
+using SiteServer.CMS.StlParser.Model;
+
+namespace SiteServer.API.Controllers.Pages.Cms
+{
+    [OpenApiIgnore]
+    [RoutePrefix("pages/cms/contents")]
+    public class PagesContentsController : ApiController
+    {
+        private const string Route = "";
+        private const string RouteCreate = "actions/create";
+
+        [HttpGet, Route(Route)]
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var request = new AuthenticatedRequest();
+
+                var siteId = request.GetQueryInt("siteId");
+                var channelId = request.GetQueryInt("channelId");
+                var page = request.GetQueryInt("page");
+
+                if (!request.IsAdminLoggin ||
+                    !request.AdminPermissionsImpl.HasChannelPermissions(siteId, channelId,
+                        ConfigManager.ChannelPermissions.ContentView))
+                {
+                    return Unauthorized();
+                }
+
+                var siteInfo = SiteManager.GetSiteInfo(siteId);
+                if (siteInfo == null) return BadRequest("无法确定内容对应的站点");
+
+                var channelInfo = ChannelManager.GetChannelInfo(siteId, channelId);
+                if (channelInfo == null) return BadRequest("无法确定内容对应的栏目");
+
+                var adminId = channelInfo.Additional.IsSelfOnly
+                    ? request.AdminId
+                    : request.AdminPermissionsImpl.GetAdminId(siteId, channelId);
+                var isAllContents = channelInfo.Additional.IsAllContents;
+
+                var pluginIds = PluginContentManager.GetContentPluginIds(channelInfo);
+                var pluginColumns = PluginContentManager.GetContentColumns(pluginIds);
+
+                var columns = ContentManager.GetContentColumns(siteInfo, channelInfo, false);
+
+                var pageContentInfoList = new List<ContentInfo>();
+                var count = ContentManager.GetCount(siteInfo, channelInfo, adminId, isAllContents);
+                var pages = Convert.ToInt32(Math.Ceiling((double)count / siteInfo.Additional.PageSize));
+                if (pages == 0) pages = 1;
+
+                if (count > 0)
+                {
+                    var offset = siteInfo.Additional.PageSize * (page - 1);
+                    var limit = siteInfo.Additional.PageSize;
+
+                    var pageContentIds = ContentManager.GetChannelContentIdList(siteInfo, channelInfo, adminId, isAllContents, offset, limit);
+
+                    var sequence = offset + 1;
+                    foreach (var channelContentId in pageContentIds)
+                    {
+                        var contentInfo = ContentManager.GetContentInfo(siteInfo, channelContentId.ChannelId, channelContentId.ContentId);
+                        if (contentInfo == null) continue;
+
+                        var menus = PluginMenuManager.GetContentMenus(pluginIds, contentInfo);
+                        contentInfo.Set("PluginMenus", menus);
+
+                        var channelName = ChannelManager.GetChannelNameNavigation(siteId, channelId, channelContentId.ChannelId);
+                        contentInfo.Set("ChannelName", channelName);
+
+                        pageContentInfoList.Add(ContentManager.Calculate(sequence++, contentInfo, columns, pluginColumns));
+                    }
+                }
+
+                var permissions = new
+                {
+                    IsAdd = request.AdminPermissionsImpl.HasChannelPermissions(siteInfo.Id, channelInfo.Id, ConfigManager.ChannelPermissions.ContentAdd) && channelInfo.Additional.IsContentAddable,
+                    IsDelete = request.AdminPermissionsImpl.HasChannelPermissions(siteInfo.Id, channelInfo.Id, ConfigManager.ChannelPermissions.ContentDelete),
+                    IsEdit = request.AdminPermissionsImpl.HasChannelPermissions(siteInfo.Id, channelInfo.Id, ConfigManager.ChannelPermissions.ContentEdit),
+                    IsTranslate = request.AdminPermissionsImpl.HasChannelPermissions(siteInfo.Id, channelInfo.Id, ConfigManager.ChannelPermissions.ContentTranslate),
+                    IsCheck = request.AdminPermissionsImpl.HasChannelPermissions(siteInfo.Id, channelInfo.Id, ConfigManager.ChannelPermissions.ContentCheck),
+                    IsCreate = request.AdminPermissionsImpl.HasSitePermissions(siteInfo.Id, ConfigManager.WebSitePermissions.Create) || request.AdminPermissionsImpl.HasChannelPermissions(siteInfo.Id, channelInfo.Id, ConfigManager.ChannelPermissions.CreatePage),
+                    IsChannelEdit = request.AdminPermissionsImpl.HasChannelPermissions(siteInfo.Id, channelInfo.Id, ConfigManager.ChannelPermissions.ChannelEdit)
+                };
+
+                return Ok(new
+                {
+                    Value = pageContentInfoList,
+                    Count = count,
+                    Pages = pages,
+                    Permissions = permissions,
+                    Columns = columns,
+                    IsAllContents = isAllContents
+                });
+            }
+            catch (Exception ex)
+            {
+                LogUtils.AddErrorLog(ex);
+                return InternalServerError(ex);
+            }
+        }
+
+        [HttpPost, Route(RouteCreate)]
+        public IHttpActionResult Create()
+        {
+            try
+            {
+                var request = new AuthenticatedRequest();
+
+                var siteId = request.GetPostInt("siteId");
+                var channelContentIds = request.GetPostObject<List<MinContentInfo>>("channelContentIds");
+
+                if (!request.IsAdminLoggin)
+                {
+                    return Unauthorized();
+                }
+
+                var siteInfo = SiteManager.GetSiteInfo(siteId);
+                if (siteInfo == null) return BadRequest("无法确定内容对应的站点");
+
+                foreach (var channelContentId in channelContentIds)
+                {
+                    CreateManager.CreateContent(siteId, channelContentId.ChannelId, channelContentId.Id);
+                }
+
+                return Ok(new
+                {
+                    Value = true
+                });
+            }
+            catch (Exception ex)
+            {
+                LogUtils.AddErrorLog(ex);
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
